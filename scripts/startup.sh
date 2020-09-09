078d396080499f6ae9263be85daeb655d7a5e3f1
Cameron Smith <camrn86@gmail.com>
2020-09-08 23:03:20 -0400

    modify startup configuration and include latin modern fonts

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,8 +3,12 @@
 # [manual] startup script 
 # > ./startup.sh
 #
-
-
+# https://cloud.google.com/compute/docs/startupscript#startupscriptrunninginstances
+# startup-script: supply the startup script contents directly by using this key.
+# startup-script-url: supply a Cloud Storage URL to the start script file by using this key.
+#
+# > HOME=/home/jovyan /home/jovyan/scripts/startup.sh
+#
 # PACKAGES to install at startup
 
 ## Arch packages
@@ -12,7 +16,7 @@ sudo pacman -Syu --needed --noconfirm - < $HOME/etc/pkglist-startup.txt
 sudo pacman -Scc --noconfirm
 
 ## Python packages
-pip install --user -r $HOME/etc/python-libraries.txt
+# pip install --user -r $HOME/etc/python-libraries.txt
 
 ## AUR packages
-yay -S --needed --noconfirm - < $HOME/etc/pkglist-yay.txt
+# yay -S --needed --noconfirm - < $HOME/etc/pkglist-yay.txt
