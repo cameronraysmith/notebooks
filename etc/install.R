934222ce0f637663786debf24f2061d0cc0f5adf
Cameron Smith <camrn86@gmail.com>
2022-02-02 09:17:03 -0700

    add maftools and movics

--- etc/install.R
+++ etc/install.R
@@ -5,15 +5,19 @@ depp <- c("BioCircos","cluster","devtools","ggplot2","enrichR","htmlwidgets",
           "shinyjs","reshape2","shinythemes","stats","purrr","readr","UpSetR")
 
 BioDepp <- c("IRanges","BiocGenerics","clusterProfiler","GenomicRanges",
-            "cBioPortalData", "AnVIL", "iClusterPlus", "MOFA2", "MOFAdata",
-             "tidyverse", "BloodCancerMultiOmics2017", "curatedTCGAData",
-             "GenomicDataCommons","SingleR")
+            "cBioPortalData","AnVIL","iClusterPlus","MOFA2","MOFAdata",
+             "tidyverse","BloodCancerMultiOmics2017","curatedTCGAData",
+             "GenomicDataCommons","SingleR","TCGAbiolinks","maftools")
 
 # Check and install missing R packages
 depp.new<-depp[!(depp%in%installed.packages())]
 if (length(depp.new)) {
   install.packages(depp.new, repos='http://cran.us.r-project.org', Ncpus = 4)
 }
+
+packageurl <- "https://cran.r-project.org/src/contrib/Archive/heatmap.plus/heatmap.plus_1.3.tar.gz"
+install.packages(packageurl, repos=NULL, type="source", Ncpus = 4)
+
 # # Check and install missing Bioconductor packages
 BioDepp.new<-BioDepp[!(BioDepp%in%installed.packages())]
 if (length(BioDepp.new)) {
@@ -21,3 +25,5 @@ if (length(BioDepp.new)) {
     install.packages("BiocManager", repos='http://cran.us.r-project.org', Ncpus = 4)
   BiocManager::install(BioDepp, type="source", Ncpus = 4)
 }
+
+devtools::install_github("xlucpu/MOVICS")
